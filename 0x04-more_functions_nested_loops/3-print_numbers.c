27901d4d282ac2e33ed0517fa87fd4a86511ddc5
MorrisGcode <morrisgithinji07@gmail.com>
2022-12-16 10:10:09 -0800

    function that prints numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+  *print_numbers -prints numbers
+  *Returns : 0 (success)
+  */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		_putchat(i);
+	}
+	_putchar('\n');
+}
+
