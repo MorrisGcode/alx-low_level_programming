4ec56f3a17bac5006f2860701ea58f4beb636e84
MorrisGcode <morrisgithinji07@gmail.com>
2022-12-16 09:51:01 -0800

    function that returns multiplication of two numbers

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *mul - multiples two numbers
+  *@a: ist number
+  *@b:second number
+  *Return: multiplied number
+  */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
