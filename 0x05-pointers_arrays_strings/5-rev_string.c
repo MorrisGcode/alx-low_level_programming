ab863c29923625b56acce6d966f7d3b1f8d3c192
MorrisGcode <morrisgithinji07@gmail.com>
2022-12-19 12:09:33 -0800

    edit function

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -9,19 +9,21 @@
 
 void rev_string(char *s)
 {
-	int len, i = 0, j = len / 2;
+	int len, i, j;
 	char c;
 
 	for (len = 0; s[len] != '\0'; len++)
 	{
-		;
-	}
+		i = 0;
+		j = len / 2;
+
 	while (j--)
 	{
 		c = s[len - i - 1];
 		s[len - i - 1] = s[i];
 		s[i] = c;
 		i++;
+	}
 	}
 }
 
