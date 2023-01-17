5a665db7845c16dfb2c03f860462b59b601e8405
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-17 03:33:50 -0800

    function that returns 1 if the input integer is a prime number, otherwise return 0

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -12,6 +12,6 @@ int _sqrt_recursion(int n)
 	if (n == 0)
 		return (-1);
 
-	return (_sqrt_recursion(return sqrt(n)));
+
 }
 
---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+  *evaluate_num - recursion loop
+  *@n: number
+  *Return 1 or 0
+  */
+
+int evaluate_n(int n, int iterator)
+{
+	if (iterator == n - 1)
+		return (10);
+
+	else if (n % iterator == 0)
+		return (0);
+
+	else if (n % iterator != 0)
+		return (evaluate_n(n, iterator + 1));
+
+	return (0);
+}
+
+int is_prime_number(int n)
+{
+	int iterator;
+
+	iterator = 2;
+
+	if (n < 2)
+		return (0);
+	else (n == 0)
+		return (1);
+
+	return (evaluate_n(n, iterator));
+}
+
+
