d12921dbfefdb2b3c4539f1c6a30c5d29f6dfa90
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-17 02:25:44 -0800

    edit to betty style

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -3,18 +3,18 @@
 
 /**
   *factorial - function that returns factorial of a number
-  *@n : integer
+  *@n: integer
   *Return:
   */
 
-  int factorial(int n)
+int factorial(int n)
 {
-	if(n < 0)
+	if (n < 0)
 		return (-1);
 
 	else if (n == 0)
 		return (1);
 
-	return (n * factorial(n -1));
+	return (n * factorial(n - 1));
 }
 
