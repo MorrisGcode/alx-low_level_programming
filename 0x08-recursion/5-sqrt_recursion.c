9ebc7b988f6d09158729ebae457eb1d19ce90940
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-17 03:17:31 -0800

    function to get squareroot

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <math.h>
+/**
+  *_sqrt_recursion - returns natural sqrt of a number
+  *@n: integer
+  *Return: sqroot(integer)
+  */
+
+int _sqrt_recursion(int n)
+{
+	if (n == 0)
+		return (-1);
+
+	return (_sqrt_recursion(return sqrt(n)));
+}
+
