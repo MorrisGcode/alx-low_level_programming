1c0b2c07d97ae5710268ed75adee62b32716fce4
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-17 02:09:03 -0800

    edit to betty style

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -3,8 +3,8 @@
 
 /**
   *_strlen_recursion - function that returns length of string
-  *@s : pointer
-  *Return : length of string(int)
+  *@s: pointer
+  *Return: display of length of string(int)
   */
 
 int _strlen_recursion(char *s)
@@ -12,5 +12,5 @@ int _strlen_recursion(char *s)
 	if (*s != '\0')
 		return (1 + _strlen_recursion(s + 1));
 
-	return(0);
+	return (0);
 }
