865dfd259be8316f43ab30546df48b84d7541d96
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-18 05:45:45 -0800

    edit file 1

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -2,7 +2,7 @@
 
 /**
   *init_dog - function that initializes struct dog
-  *@d;
+  *@d: pointer to user defined data type
   *@name: pointer to dog's name
   *@age: pointer to dog's age
   *@owner: pointer to dog's owner
@@ -12,7 +12,6 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d)
-
 	{
 		d->name = name;
 		d->age = age;
