876c139fbb537e3df06d495d11b4de94589d829b
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-18 05:01:28 -0800

    edit to betty style

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+  *struct dog - user defined data type
+  *@name: name of dog
+  *@age: age of dog
+  *@owner : owner of the dog
+  */
+
 struct dog
 {
 	char *name;
-        float age;
+	float age;
 	char *owner;
 };
 
