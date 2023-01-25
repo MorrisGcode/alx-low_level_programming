928984fd20dcb235c4c52b10c3408fc7598be75d
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-25 04:24:14 -0800

    call function to act as array

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  *array_iterator - calls a function to act as array elements
+  *@array: Name of the array
+  * @size: size of the passed array to function
+  * @action: the function that acts on the array elements
+  * Return: VOID
+  */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	int i;
+
+	if (!array || !action)
+		return;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
