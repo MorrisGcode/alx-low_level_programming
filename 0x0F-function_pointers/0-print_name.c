c648613ae61d7721ddc896389ba1ce5f528eaa00
MorrisGcode <morrisgithinji07@gmail.com>
2023-01-25 04:07:10 -0800

    function to print name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+  *print_name - function that prints a name
+  *@name: name of the person
+  *@f : pointer
+  *Return : none
+  */
+
+void print_name_as_is(char *name)
+{
+	if (!name || !f)
+		return;
+
+	f(name);
+}
